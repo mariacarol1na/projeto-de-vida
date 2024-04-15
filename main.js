ce79e55da4a2690cc0f44e25414d0dd0ffa4418e
mariacarol1na <128617169+mariacarol1na@users.noreply.github.com>
2024-04-15 08:50:10 -0300

    ok

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
+const textos=document.querySelectorAll(".aba-conteudo");
 
 for(let i=0; i<botoes.length;i++){
     botoes[i].onclick=function(){
 
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
     botoes[i].classList.add("ativo");  
+    textos[i].classList.add ("ativo");
       }
 }
