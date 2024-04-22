928228bf9ff07fe155590f4f63ea77e9b01f3b0b
mariacarol1na <128617169+mariacarol1na@users.noreply.github.com>
2024-04-22 08:41:42 -0300

    ok

--- main.js
+++ main.js
@@ -11,5 +11,31 @@ for(let i=0; i<botoes.length;i++){
 
     botoes[i].classList.add("ativo");  
     textos[i].classList.add ("ativo");
-      }
+    
+    const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-13T23:59:59");
+const tempoObjetivo2 = new Date("2024-11-30T23:59:59");
+const tempoObjetivo3 = new Date("2024-09-01T23:59:59");
+const tempoObjetivo4 = new Date("2024-07-30T23:59:59");
+
+const tempos=[tempoObjetivo1, tempoObjetivo2, tempoObjetivo3,tempoObjetivo4];
+for (let i=0; i < contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);  
+}
+
+contadores[0].textContent = calculaTempo (tempoObjetivo1);
+
+function calculaTempo (tempoObjetivo) {
+    let tempoAtual = new Date();
+let tempoFinal=tempoObjetivo - tempoAtual;
+let segundos=Math.floor (tempoFinal/1000);
+let minutos=Math.floor (segundos/60);
+let horas=Math.floor (minutos/60);
+let dias=Math.floor (horas/24);
+
+segundos %=60;
+minutos %=60;
+horas %=24return dias+" dias "+ horas+" horas "+minutos+" minutos "+segundos+" segundos ";}
+
+}
 }
